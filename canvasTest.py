92f400801e13781044d223f0908a0dea2fb724f6
seyeon325 <ho78455@gmail.com>
2025-04-07 10:17:32 +0900

    weeks - 5

---
+++ canvasTest.py
@@ -0,0 +1,19 @@
+import tkinter
+
+# 루트 객체 생성
+root = tkinter.Tk()
+root.title("첫 번째 캔버스")
+
+# 캔버스 생성
+canvas = tkinter.Canvas(root, width=400, height=600, bg="skyblue")
+canvas.pack()
+
+# 캔버스 내 이미지 생성
+gazou = tkinter.PhotoImage(file="miko.png")
+canvas.create_image(200, 300, image=gazou)
+
+# 메인 루프 실행
+root.mainloop()
+
+
+#루트 -> 캔버스 -> 백그라운드
