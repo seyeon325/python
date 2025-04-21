b7eeacb97fc2ae0a8b55ea9e274c10baad7ee271
seyeon325 <ho78455@gmail.com>
2025-04-21 10:49:11 +0900

    시험 문제

---
+++ 문자열_실습_트와이스.py
@@ -0,0 +1,8 @@
+strIn = input("원본 문자열 ==> ") #4자리 문자열트
+strOut = strIn[3]+strIn[2]+strIn[1]+strIn[0]
+
+#반복문
+n = len(strIn)
+print(n)
+
+print("반대 문자열 ==>",strOut)
---
+++ 입력,출력_실습 3 택배.py
@@ -0,0 +1,7 @@
+print("## 택배를 보내기 위한 정보를 입력하세요.")
+name = input(" 받는 사람 :")
+address = input(" 주소 :")
+weight=  input(" 무게(g) :")
+print( "** 받는 사람 ==>", name)
+print( "** 주소 ==>", address)
+print( "** 무게 ==>", weight)
